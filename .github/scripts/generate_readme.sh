b9d3967c914b65fad5d33dee316083f157c62ca4
Louis Smith <15144861+smudger@users.noreply.github.com>
2022-12-30 13:09:03 +0000

    Update GitHub Action

--- .github/scripts/generate_readme.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cp .github/data/README.adoc README.adoc
-
-# Replace `include` directive` with file contents, as GitHub does not support AsciiDoc `include` directives.
-sed -i "" "s/include\:\:benchmark\.csv\[\]/$(sed -e 's/[\&/]/\\&/g' -e 's/$/\\n/' .github/data/benchmark.csv | tr -d '\n')/" README.adoc
